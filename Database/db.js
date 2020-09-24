f92f6544a353aa0e74cc68dfcf17bcef4452445b
owegami <busimeiru@gmail.com>
2020-09-24 12:21:57 -0700

    "Created initial schema"

--- Database/db.js
+++ Database/db.js
@@ -5,3 +5,9 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', console.log('Connection made to MongoDB pictures database!'));
 
+const picture = new mongoose.Schema({
+  _id: Number,
+	imgUrl: String,
+  uploadDate: Number,
+	user: String
+})
