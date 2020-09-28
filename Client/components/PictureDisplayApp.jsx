64876d01470b0bd844fffb5769fe1e120f478bce
owegami <busimeiru@gmail.com>
2020-09-28 14:28:50 -0700

    "Set up styled components"

--- Client/components/PictureDisplayApp.jsx
+++ Client/components/PictureDisplayApp.jsx
@@ -5,7 +5,43 @@ import MainPic from './mainPic.jsx';
 import GridPics from './gridPics.jsx';
 import SidebarPics from './sidebarPics.jsx';
 
+//Styling
+const PictureContainer = styled.div`
+display: grid;
+grid-template-columns: repeat(13, 60px, [col-start]);
+grid-template-rows: repeat(10, 50px, [row-start]);
+background-color: green;
+width: 818px;
+height: 481px;
+grid-template-areas:
+"main main main main main main main main main main viewOptions viewOptions viewOptions"
+"main main main main main main main main main main viewOptions viewOptions viewOptions"
+"main main main main main main main main main main viewOptions viewOptions viewOptions"
+"main main main main main main main main main main viewOptions viewOptions viewOptions"
+"main main main main main main main main main main viewOptions viewOptions viewOptions"
+"main main main main main main main main main main viewOptions viewOptions viewOptions"
+"main main main main main main main main main main viewOptions viewOptions viewOptions"
+"main main main main main main main main main main viewOptions viewOptions viewOptions"
+"gallery gallery gallery gallery gallery gallery gallery gallery gallery gallery viewOptions viewOptions viewOptions"
+"gallery gallery gallery gallery gallery gallery gallery gallery gallery gallery viewOptions viewOptions viewOptions";
+`
 
+const PictureMainViewer = styled.div`
+  grid-area: main;
+  background-color: teal;
+`
+
+const PictureMiniGrid = styled.div`
+  grid-area: gallery;
+  background-color: orange;
+`
+
+const PictureSideGrid = styled.div`
+  grid-area: viewOptions;
+  background-color: blueviolet;
+`;
+
+//App itself
 class PictureDisplayApp extends React.Component {
   constructor (props) {
     super(props);
@@ -14,19 +50,18 @@ class PictureDisplayApp extends React.Component {
   render () {
 
     return (
-      <div className="picture-container">
-        <div id="picture-mainViewer">
+      <PictureContainer>
+        <PictureMainViewer>
           <MainPic />
-        </div>
-        <div id="picture-miniGrid">
+        </PictureMainViewer>
+        <PictureMiniGrid>
           <GridPics />
-        </div>
-
-        <div id="picture-sideGrid">
+        </PictureMiniGrid>
+        <PictureSideGrid>
           <SidebarPics />
-        </div>
+        </PictureSideGrid>
 
-      </div>
+      </PictureContainer>
     );
   }
 
