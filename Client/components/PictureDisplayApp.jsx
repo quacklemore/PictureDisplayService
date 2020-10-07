a7841017f3b6ec24589948d12a8c98d364a3dc57
owegami <busimeiru@gmail.com>
2020-10-07 12:52:17 -0700

    "Refactor Picture Display to allow for popOut module"

--- Client/components/PictureDisplayApp.jsx
+++ Client/components/PictureDisplayApp.jsx
@@ -4,10 +4,9 @@ import axios from 'axios';
 import _ from 'underscore';
 
 import MainPic from './mainPic.jsx';
+import PopUpWindow from './PopoutWindow.jsx'
 import SidebarPics from './sidebarPics.jsx';
 import Thumbnail from './thumbnail.jsx';
-import PopOutFlexPics from './popOutFlexGridPics.jsx';
-import AlbumProcessor from './AlbumIntercept.jsx';
 
 //Styling
 
@@ -49,122 +48,7 @@ const PictureSideGrid = styled.div`
   flex-basis: 25%;
   max-width: 200px;
   min-width: 170px;
-`;
-
-//styling pop out window
-const PopOutWindowOfPics = styled.div`
-  position: absolute;
-  top: 2.5%;
-  left: 2.5%;
-  width: 95vw;
-  height: 95vh;
-  background-color: white;
-  opacity: 0%;
-  z-index: -2;
-  box-shadow: 10px 10px 15px grey;
-  overflow: auto;
-  font-family: 'Quicksand';
-`;
-
-const ClosePopUpButton = styled.button`
-  position: absolute;
-  width: 40px;
-  height: 40px;
-  top: 0%;
-  right: 0%;
-  border: .5px dotted grey;
-  text-align: center;
-  background-color: white;
-  font-size: 1.5em;
-  font-weight: bold;
-  overflow: hidden;
-  z-index: 1020;
-`;
-
-const GreyOutBackground = styled.div`
-  position: absolute;
-  top: 0px;
-  left: 0px;
-  width: 0.01%;
-  height: 0.01%;
-  opacity: 0%;
-  background-color: grey;
-  z-index: -2;
-`;
-
-const PopUpFlexWrappers = styled.div`
-  position: absolute;
-  display: flex;
-  flex-direction: column;
-  flex-wrap: wrap;
-  width: 100%;
-  height: 100%;
-  top: 0px;
-  left: 0px;
-`;
-
-const PopOutWindowFlex = styled.div`
-  position: absolute;
-  display: flex;
-  flex-flow: column wrap;
-  flex-direction: row;
-  flex-wrap: wrap;
-  align-content: stretch;
-  object-fit: contain;
-  overflow: auto;
-  width: 80%;
-  height: 85%;
-  top: 6%;
-  right: 0%;
-`;
-
-const AlbumWrapper = styled.div`
-  position: absolute;
-  display: block;
-  flex-wrap: wrap;
-  justify-content: center;
-  overflow: scroll;
-  width: 20%;
-  height: 100%;
-  background-color: #e5e5e5;
-`;
-
-const PopOutWindowAlbumGrid = styled.div`
-  position: relative;
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  align-content: flex-start;
-  justify-content: center;
-  overflow: hidden;
-  margin: 5%;
-  width: 90%;
-  height: 100%;
-`;
-
-const FlexPicWrapperSmalls = styled.div`
-  margin: 5px;
-  width: 30%;
-`;
-
-const FlexPicWrapperFull = styled.div`
-  width: 100%;
-  height: 100%;
-`;
-
-const MainHeaderOfText = styled.div`
-  font-weight: bolder;
-  text-decoration: underline;
-  text-align: left;
-  width: 80%;
-  height: 5%;
-  font-size: 1.2em;
-  padding-left: 20px;
-`;
-
-const HeaderOfText = styled.span`
-  font-weight: bold;
-  text-decoration: underline;
+  z-index: 1;
 `;
 
 //App itself
@@ -325,7 +209,7 @@ class PictureDisplayApp extends React.Component {
 
       }
     } else if (comp === 'tag') {
-      let tagArray = this.state.tags[tag];
+      let tagArray = this.state.tags[tag] === undefined ? [] : this.state.tags[tag];
       let tagPhotoArray = tagArray.map((photo) => {
         return photo.imgMainUrl;
       })
@@ -430,42 +314,17 @@ class PictureDisplayApp extends React.Component {
       <div>
         {/* pop out window business */}
         {/* ReactDom.createPortal( */}
-        <PopOutWindowOfPics id='picturePopOutWindowOfPics'>
-          <ClosePopUpButton onClick={this.toggleWindowClosed.bind(this)}>&#x2573;</ClosePopUpButton>
-          <PopUpFlexWrappers>
-            <AlbumWrapper>
-              <PopOutWindowAlbumGrid>
-                <HeaderOfText>Albums Based On Tags: </HeaderOfText>
-                <AlbumProcessor tags={this.state.tags.albums} changeContent={this.setWindowContent.bind(this)}/>
-              </PopOutWindowAlbumGrid>
-            </AlbumWrapper>
-            <PopOutWindowFlex>
-              <MainHeaderOfText>{this.state.flexPicsIs}</MainHeaderOfText>
-              {
-                this.state.flexedPics.map((photo, index) => {
-                  if (this.state.isFullSize) {
-                    return (
-                      <FlexPicWrapperFull key={'popOut' + index} id={'FlexPicWrapper'}>
-                        <PopOutFlexPics photo={photo}  id={index} changePic={() => {
-                          this.changeMainPic(event, null , this.setWindowContent.bind(this))
-                        }} picId={this.state.mainPhotoId > 0 ? this.state.mainPhotoId - 1 : this.state.photos.length - 1}/>
-                      </FlexPicWrapperFull>
-                    )
-                  } else {
-                    return (
-                      <FlexPicWrapperSmalls key={'popOut' + index} id={'FlexPicWrapper'}>
-                        <PopOutFlexPics photo={photo} changePhoto={() => {
-                          this.changeMainPic(null, photo, this.setWindowContent.bind(this))
-                        }}/>
-                      </FlexPicWrapperSmalls>
-                    )
-                  }
-                })
-              }
-            </PopOutWindowFlex>
-          </PopUpFlexWrappers>
-        </PopOutWindowOfPics>
-        <GreyOutBackground id='pictureGreyOutBackground' onClick={this.toggleWindowClosed}/>
+        <PopUpWindow
+          toggleWindowClosed={this.toggleWindowClosed.bind(this)}
+          tags={this.state.tags.albums}
+          setWindowContent={this.setWindowContent.bind(this)}
+          flexPicsIs={this.state.flexPicsIs === undefined ? [] : this.state.flexPicsIs}
+          isFullSize={this.state.isFullSize}
+          changeMainPic={this.changeMainPic.bind(this)}
+          mainPhotoId={this.state.mainPhotoId === undefined ? '' : this.state.mainPhotoId}
+          photos={this.state.photos === undefined ? [] : this.state.mainPhotoId}
+          />
+
         {/* , document.getElementById('pictureDisplayPortal')) */}
 
         {/* Main module business */}
