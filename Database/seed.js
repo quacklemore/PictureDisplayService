600a2f9f83792e4c0c06d24f0589e3d01c8a490b
owegami <busimeiru@gmail.com>
2020-10-09 18:44:05 -0700

    "Fix the DB for randomness in all ways"

--- Database/seed.js
+++ Database/seed.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const db = require('./db.js');
 const faker = require('faker');
+const _ = require('underscore');
 const Photo = require('./Photo.js');
 
 const saveOnePhoto = (image) => {
@@ -17,30 +18,50 @@ const saveOnePhoto = (image) => {
 
 
 const seed = () => {
-  Photo.deleteMany({});
   let pictures = [];
 
   for (let i = 0; i < 100; i++) {
     let hotelName = 'hotel' + i;
-    let randNum = Math.floor(Math.random() * 58 + 25);
+    let randNum = (25 + Math.random() * (58 - 25));
     let bool = (Math.random() > 0.5) ? true : false;
     let specialObj = {};
     let gamble = Math.random();
-    let arrayOfRandom = Math.floor(Math.random() * randNum + 1);
 
-    for (let x = 1; x <= randNum; x++) {
-      let tags = ['dogs', 'beach', 'sunshine', 'Wonderful', 'Good Food', 'happy', 'sandwich', 'beach life', 'Perfection', 'Okay', 'Passable', 'Overpriced', 'Meh', 'Too Sunny', 'Glaringly Beautiful'];
-      let imageNum = arrayOfRandom[x];
-      let tagsNum = Math.floor(Math.random() * tags.length);
+    let totalNumberOfPictures = Array.from(Array(58).keys());
+    let arrayOfRandom = totalNumberOfPictures.map((nothing, index) => {
+    let randNum = (Math.floor(Math.random() * 58 +1));
+      return randNum;
+    });
+    arrayOfRandom = _.uniq((arrayOfRandom));
+    let length = arrayOfRandom.length;
+
+    let tags = ['dogs', 'beach', 'sunshine', 'Wonderful', 'Good Food', 'happy', 'sandwich', 'beach life', 'Perfection', 'Okay', 'Passable', 'Overpriced', 'Meh', 'Too Sunny', 'Glaringly Beautiful'];
+    let tagsNum = Math.floor(Math.random() * tags.length);
+    let tagsNumArray = [];
+    for (let i = 0; i < tagsNum; i++) {
+      tagsNumArray.push(Math.floor(Math.random() * tags.length))
+    }
+
+    tagsForThisHotel = tags.map((tag, index) => {
+      if (tagsNumArray.indexOf(index) !== -1) {
+        return tag;
+      } else {
+        return;
+      }
+    })
+
+    for (let x = 1; x <= length; x++) {
+      let imageNum = arrayOfRandom.pop();
       let image = {};
       image.imgMainUrl = `https://tripadcoba.s3-us-west-1.amazonaws.com/main${imageNum}.jpg`;
       image.imgFullUrl = `https://tripadcoba.s3-us-west-1.amazonaws.com/full${imageNum}.jpg`;
       image.imgThumbUrl = `https://tripadcoba.s3-us-west-1.amazonaws.com/thumb${imageNum}.jpg`;
 
+
       image.uploadDate = new Date();
       image.user = faker.name.firstName() + faker.name.lastName();
       image.hotel = hotelName;
-      image.tag = tags[tagsNum];
+      image.tag = tagsForThisHotel[Math.floor(Math.random() * tagsForThisHotel.length)];
       specialObj.is = false;
 
       //not currently in use
